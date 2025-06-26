7a9ce374c78c7757e2e85dbb066a63126876e418
ahiehe <ahiehe@outlook.com>
2025-06-26 14:30:39 +0200

    Changed structure

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import {SandSimulationPage} from "../pages/SandSimulationPage";
-import {MainSandGridProvider} from "../components/MainSandGridProvider";
-import {BrushProvider} from "../components/BrushProvider";
-import {ControlsProvider} from "../components/ControlsProvider";
+import {SandSimulationPage} from "./pages/SandSimulationPage";
+import {MainSandGridProvider} from "./components/MainSandGridProvider";
+import {BrushProvider} from "./components/BrushProvider";
+import {ControlsProvider} from "./components/ControlsProvider";
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
